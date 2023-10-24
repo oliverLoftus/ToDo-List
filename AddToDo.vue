bba5dd5d90dc11c4040eb71269145413ec395859
oliverLoftus <67932990+oliverLoftus@users.noreply.github.com>
2023-10-24 12:04:45 +0100

    Add files via upload

---
+++ AddToDo.vue
@@ -0,0 +1,28 @@
+<!-- src/components/AddToDo.vue -->
+<templates>
+    <div>
+        <h2>Add ToDo</h2>
+        <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Enter a new ToDo" />
+        <button @click="addTodo">Add</button>
+    </div>
+</templates>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const newTodo = ref('');
+        const addTodo = () => {
+            if (newTodo.value.trim() === '') return;
+            todos.value.push(newTodo.value);
+            newTodo.value = '';
+        };
+
+        return {
+            newTodo,
+            addTodo,
+        };
+    },
+};
+</script>
