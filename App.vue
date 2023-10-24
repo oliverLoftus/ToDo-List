38b852d656a467eecf65c475b6127a49bd4345b9
oliverLoftus <67932990+oliverLoftus@users.noreply.github.com>
2023-10-24 12:30:12 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<!-- src/App.vue -->
+<template>
+    <div>
+        <h1>Vue 3 ToDo App</h1>
+        <add-todo />
+        <to-do-list />
+    </div>
+</template>
+
+<script>
+import AddTodo from './components/AddToDo.vue';
+import ToDoList from '.components/ToDoList.vue';
+
+export default {
+    components: {
+        AddTodo,
+        ToDoList,
+    },
+};
+</script>
