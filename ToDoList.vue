2109ce363d1387e0b466812e3f1262db5f17356d
oliverLoftus <67932990+oliverLoftus@users.noreply.github.com>
2023-10-24 13:46:04 +0100

    Add files via upload

---
+++ ToDoList.vue
@@ -0,0 +1,31 @@
+<!-- src/components/ToDoList.vue -->
+<templates>
+  <div>
+    <h1>ToDo list</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{todo}}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+     </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const todos = ref ([]);
+
+    const removeTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    return {
+      todos,
+      removeTodo,
+    };
+  },
+};
+</script>
